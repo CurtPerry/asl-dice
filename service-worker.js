f9843afe395cfe343ce0f2898172149f8fd9bfea
CurtPerry <128939758+CurtPerry@users.noreply.github.com>
2026-06-15 13:55:50 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const cacheName = "asl-dice-v4";
+const appShell = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appShell)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
